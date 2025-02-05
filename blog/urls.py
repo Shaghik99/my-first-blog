0ecbf0ea3950f915f305b5511b02c013c41369f8
shaghik <sh.torosian@gmail.com>
2025-02-06 00:50:54 +0400

    url paths added

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('', views.nature, name='nature'),
+    path('', views.street, name='street'),
     ]
